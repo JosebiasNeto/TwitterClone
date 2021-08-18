28de35299864d5d19dbb7b068ac1d5eeb1c3232f
Josebias Neto <josebiasbsneto@gmail.com>
2021-08-17 22:28:42 -0300

    Create recycler view adapter

--- app/src/main/java/com/example/twitterclone/MainActivity.kt
+++ app/src/main/java/com/example/twitterclone/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.twitterclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.twitterclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var tweetsAdapter: TweetsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val tweets = arrayListOf<Tweet>()
+        tweetsAdapter = TweetsAdapter(tweets)
+        binding.rvTweets.adapter = tweetsAdapter
+        binding.rvTweets.layoutManager = LinearLayoutManager(applicationContext)
     }
 }
---
+++ app/src/main/java/com/example/twitterclone/Tweet.kt
@@ -0,0 +1,12 @@
+package com.example.twitterclone
+
+class Tweet (
+    val tweetImage: Int,
+    val tweetName: String,
+    val tweetLogin: String,
+    val tweetTime: Int,
+    val tweetDescription: String,
+    val tweetComents: Int,
+    val tweetRetweets: Int,
+    val tweetFavoriteds: Int
+        )
